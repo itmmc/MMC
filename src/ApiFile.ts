60caf4bb700e60cd495c100d8e8dca3eceaf3cf8
Adeel Asghar <adeelasghar9292@gmail.com>
2024-05-02 15:58:34 +0500

    changes

--- src/ApiFile.ts
+++ src/ApiFile.ts
@@ -1,6 +1,6 @@
 // const baseUrl1 = 'https://mmc-server.vercel.app/api'
-// const baseUrl = 'https://mmc-backend-rho.vercel.app/api'
-const baseUrl = 'https://mmc-backend-git-test-modernmills.vercel.app/api'
+const baseUrl = 'https://mmc-backend-rho.vercel.app/api'
+// const baseUrl = 'https://mmc-backend-git-test-modernmills.vercel.app/api'
 // const baseUrl = 'https://mmc-backend-git-dev-modernmills.vercel.app/api'
 
 export default baseUrl
